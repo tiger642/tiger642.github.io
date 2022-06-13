267205db2fe9734e993576b2f1e4b517971daeed
tiger642 <84329201+tiger642@users.noreply.github.com>
2022-06-13 12:41:45 -0500

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,77 @@
+const display = document.getElementById('display');
+
+const menu = document.getElementById('menu').addEventListener('click', function() {
+    console.log("it works")
+})
+
+const equals = document.getElementById('equals').addEventListener('click', function() {
+    
+})
+
+const clear = document.getElementById('clear').addEventListener('click', function() {
+    
+})
+
+const positiveNegative = document.getElementById('positive-negative').addEventListener('click', function() {
+
+})
+
+const squareRoot = document.getElementById('square-root').addEventListener('click', function() {
+    
+})
+
+const backspace = document.getElementById('clear').addEventListener('click', function() {
+    
+})
+
+const multiply = document.getElementById('multply').addEventListener('click', function() {
+    
+})
+
+const divide = document.getElementById('divide').addEventListener('click', function() {
+    
+})
+
+const one = document.getElementById('one').addEventListener('click', function() {
+    
+})
+
+const two = document.getElementById('two').addEventListener('click', function() {
+    
+})
+
+const three = document.getElementById('three').addEventListener('click', function() {
+    
+})
+
+const four = document.getElementById('four').addEventListener('click', function() {
+    
+})
+
+const five = document.getElementById('five').addEventListener('click', function() {
+    
+})
+
+const six = document.getElementById('six').addEventListener('click', function() {
+    
+})
+
+const seven = document.getElementById('seven').addEventListener('click', function() {
+    
+})
+
+const eight = document.getElementById('eight').addEventListener('click', function() {
+    
+})
+
+const nine = document.getElementById('nine').addEventListener('click', function() {
+    
+})
+
+const zero = document.getElementById('zero').addEventListener('click', function() {
+    
+})
+
+const period = document.getElementById('period').addEventListener('click', function() {
+    
+})
