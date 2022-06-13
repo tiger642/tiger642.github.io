5754b0f64ba874aa3581cc29cc9c418e9214fa0b
tiger642 <84329201+tiger642@users.noreply.github.com>
2022-06-13 15:31:04 -0500

    Add files via upload

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,77 +1,118 @@
 const display = document.getElementById('display');
 
-const menu = document.getElementById('menu').addEventListener('click', function() {
-    console.log("it works")
+function displayValue(value) {
+    display.textContent += value;
+}
+
+const leftPar = document.getElementById('left-par').addEventListener('click', function() {
+    let value = '(';
+    displayValue(value);
+})
+
+const rightPar = document.getElementById('right-par').addEventListener('click', function() {
+    let value = ')';
+    displayValue(value);
 })
 
 const equals = document.getElementById('equals').addEventListener('click', function() {
-    
+    let value = display.textContent;
+    let answer = eval(value);
+    display.textContent = answer;
 })
 
 const clear = document.getElementById('clear').addEventListener('click', function() {
-    
+    display.textContent = "";
 })
 
-const positiveNegative = document.getElementById('positive-negative').addEventListener('click', function() {
-
+const square = document.getElementById('square').addEventListener('click', function() {
+    let value = display.textContent;
+    let answer = eval(value * value);
+    display.textContent = answer;
 })
 
 const squareRoot = document.getElementById('square-root').addEventListener('click', function() {
-    
+    let value = display.textContent;
+    let answer = Math.sqrt(value);
+    display.textContent = answer;
 })
 
-const backspace = document.getElementById('clear').addEventListener('click', function() {
-    
+const backspace = document.getElementById('backspace').addEventListener('click', function() {
+    let value = display.textContent;
+    value = value.substring(0, value.length - 1);
+    display.textContent = value;
 })
 
-const multiply = document.getElementById('multply').addEventListener('click', function() {
-    
+const multiply = document.getElementById('multiply').addEventListener('click', function() {
+    let value = '*';
+    displayValue(value);
 })
 
 const divide = document.getElementById('divide').addEventListener('click', function() {
-    
+    let value = '/';
+    displayValue(value);
+})
+
+const plus = document.getElementById('plus').addEventListener('click', function() {
+    let value = '+';
+    displayValue(value);
+})
+
+const minus = document.getElementById('minus').addEventListener('click', function() {
+    let value = '-'
+    displayValue(value);
 })
 
 const one = document.getElementById('one').addEventListener('click', function() {
-    
+    let value = 1;
+    displayValue(value);
 })
 
 const two = document.getElementById('two').addEventListener('click', function() {
-    
+    let value = 2;
+    displayValue(value);
 })
 
 const three = document.getElementById('three').addEventListener('click', function() {
-    
+    let value = 3;
+    displayValue(value);
 })
 
 const four = document.getElementById('four').addEventListener('click', function() {
-    
+    let value = 4;
+    displayValue(value);
 })
 
 const five = document.getElementById('five').addEventListener('click', function() {
-    
+    let value = 5;
+    displayValue(value);
 })
 
 const six = document.getElementById('six').addEventListener('click', function() {
-    
+    let value = 6;
+    displayValue(value);
 })
 
 const seven = document.getElementById('seven').addEventListener('click', function() {
-    
+    let value = 7;
+    displayValue(value);
 })
 
 const eight = document.getElementById('eight').addEventListener('click', function() {
-    
+    let value = 8;
+    displayValue(value);
 })
 
 const nine = document.getElementById('nine').addEventListener('click', function() {
-    
+    let value = 9;
+    displayValue(value);
 })
 
 const zero = document.getElementById('zero').addEventListener('click', function() {
-    
+    let value = 0;
+    displayValue(value);
 })
 
 const period = document.getElementById('period').addEventListener('click', function() {
-    
+    let value = '.';
+    displayValue(value);
 })
